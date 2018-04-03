da2025b1b91f77b190a37c4a464ee95353bae2d6
Gabriel Vidal <gabrielvidals@gmail.com>
2018-04-03 01:09:04 -0300

    add courses CRUD

---
+++ src/migrations/20180403021414-create-course.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      code: {
+        type: Sequelize.STRING,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down(queryInterface) {
+    return queryInterface.dropTable('courses');
+  },
+};
