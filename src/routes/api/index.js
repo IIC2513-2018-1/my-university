310a943a024fd877448487e15d8c65dd862ca267
Gabriel Vidal <gabrielvidals@gmail.com>
2018-05-23 22:54:36 -0400

    add api endpoints for courses

---
+++ src/routes/api/index.js
@@ -0,0 +1,9 @@
+const KoaRouter = require('koa-router');
+
+const coursesApi = require('./courses');
+
+const router = new KoaRouter();
+
+router.use('/courses', coursesApi.routes());
+
+module.exports = router;
